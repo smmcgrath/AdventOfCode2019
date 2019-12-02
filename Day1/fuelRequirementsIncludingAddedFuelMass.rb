51cc78cffcb4b197c20b874b8e33a3d8cb5dfec7
Shane Michael McGrath <30355322+smmcgrath@users.noreply.github.com>
2019-12-02 10:48:26 +0000

    chore: remove outputs puts

--- Day1/fuelRequirementsIncludingAddedFuelMass.rb
+++ Day1/fuelRequirementsIncludingAddedFuelMass.rb
@@ -7,10 +7,8 @@ end
 File.open('inputs.txt').each do |line|
     reqs = fuel_reqs(line)
     sum_fuel_needs += reqs
-    puts sum_fuel_needs
     while reqs >= 9
         reqs = fuel_reqs(reqs)
-        puts reqs
         sum_fuel_needs += reqs
     end
 end
